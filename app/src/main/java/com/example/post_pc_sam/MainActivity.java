92454db8cbe2e4dc7e26c7b6344799d376661ef1
sam <s.rosenbaum2@gmail.com>
2020-04-21 00:30:54 +0300

    first commit before ex1: remove ex0 code. start fresh

--- app/src/main/java/com/example/post_pc_sam/MainActivity.java
+++ app/src/main/java/com/example/post_pc_sam/MainActivity.java
@@ -4,29 +4,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
-import com.skyfishjy.library.RippleBackground;
 
 public class MainActivity extends AppCompatActivity {
-    public boolean animation_on = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final RippleBackground rippleBackground=(RippleBackground)findViewById(R.id.content);
-        ImageView imageView=(ImageView)findViewById(R.id.centerImage);
-        imageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (animation_on){
-                    rippleBackground.stopRippleAnimation();
-                }
-                else{
-                    rippleBackground.startRippleAnimation();
-                }
-                animation_on = !animation_on;
-            }
-        });
-
-
     }
 }
